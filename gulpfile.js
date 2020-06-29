cd8b87e14453ff29149ceb1409d0df58fa22387f
KirichenkoOleg <strannik.kirichenko136@gmail.com>
2020-06-29 21:41:19 +0300

    add image in PJpeg

--- gulpfile.js
+++ gulpfile.js
@@ -54,7 +54,7 @@ function css() {
 		.pipe(sourcemaps.init())// активация sourcemaps
 		.pipe(sass().on('error', sass.logError))
 		.pipe(concat('style.css'))
-		.pipe(cleanCSS())
+		// .pipe(cleanCSS())
 		.pipe(sourcemaps.write())// активация sourcemaps
 		.pipe(dest(path.build.css))
 		.pipe(reload({ stream: true }));
@@ -112,7 +112,7 @@ const server = series(build, docs, parallel(watcher, browser_Sync));
 exports.html = html;
 exports.css = css;
 exports.script = script;
-exports.images = images;
+exports.img = images;
 exports.clean = cleanFolder;
 exports.docs = docs;
 exports.build = build;
